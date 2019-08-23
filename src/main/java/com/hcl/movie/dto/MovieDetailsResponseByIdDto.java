0ea6c1a4e2376a87e7b52271f1872012e891e5fc
Gurpreet Singh Gohir <gurpreet.gohir@gmail.com>
2019-08-23 14:03:36 +0530

    first by gurpreet

---
+++ src/main/java/com/hcl/movie/dto/MovieDetailsResponseByIdDto.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.hcl.movie.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Gurpreet Singh
+ *
+ */
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+
+public class MovieDetailsResponseByIdDto implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer theatreId;
+//	private 
+
+}
