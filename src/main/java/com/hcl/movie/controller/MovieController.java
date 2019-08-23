22dc8141b4871a2d4814c1d4f562635797899f75
squard4ing@gmail.com <venky.svreddy@gmail.com>
2019-08-23 14:03:50 +0530

    first

--- src/main/java/com/hcl/movie/controller/MovieController.java
+++ src/main/java/com/hcl/movie/controller/MovieController.java
@@ -1,16 +1,36 @@
 package com.hcl.movie.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.movie.dto.MovieResponseDto;
+import com.hcl.movie.entity.Movie;
+import com.hcl.movie.service.MovieService;
 
 @RestController
 @CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
 @RequestMapping("/api")
 public class MovieController {
 
+	
+	@Autowired
+	MovieService movieService;
+	
 	public String hello() {
 		return "success";
+	}
+	
+	public ResponseEntity<List<Movie>> getMoviesList(){
+		
+		List<MovieResponseDto> response = movieService.getAllMovies();
+		
+		return  new ResponseEntity<>(response , HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/hcl/movie/service/MovieServiceImpl.java
+++ src/main/java/com/hcl/movie/service/MovieServiceImpl.java
@@ -1,5 +1,37 @@
 package com.hcl.movie.service;
 
-public class MovieServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.movie.dto.MovieResponseDto;
+import com.hcl.movie.repository.MovieRepository;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+
+	private static Logger logger = LoggerFactory.getLogger(MovieServiceImpl.class);
+	@Autowired MovieRepository movieRepository;
+	@Override
+	public List<MovieResponseDto> getAllMovies() {
+		logger.info("inside the getAllCategories method");
+		List<MovieResponseDto> responseList = new ArrayList<>();
+		List<MovieResponseDto> categoryList = movieRepository.findAll();
+		if(categoryList.isEmpty()) {
+			
+			throw new CategoryNotFoundException("categories not found");
+		}else
+		categoryList.stream().forEach(c ->{
+			BusinessCategoryResponseDto response = new BusinessCategoryResponseDto();
+			BeanUtils.copyProperties(c,response);
+			responseList.add(response);
+		});
+		return responseList;
+	}
 
 }
